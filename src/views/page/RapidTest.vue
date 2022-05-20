326b26ec0e3c7a8fd475efa59114457eda7cfd86
mizu <psdk55052004@gmail.com>
2022-05-21 01:20:20 +0800

    修改 快篩頁沒有收藏藥局的提示，從 v-if 改為 d-none 測試看看，update_12 氣象、疫情查詢 2022/5/21

--- src/views/page/RapidTest.vue
+++ src/views/page/RapidTest.vue
@@ -64,7 +64,7 @@
       <p class="card-text">備註：{{ pharmacyInfo[9] }}</p>
     </div>
   </section>
-  <div class="text-center" v-if="collectTipShow">
+  <div class="text-center" :class="{'d-none': collectTipHide}">
     <h3 class="mb-3">您目前沒有收藏的藥局！</h3>
     <button type="button" class="btn btn-color-primary-m" @click="$openTipModal('rapidTest')">收藏藥局教學</button>
   </div>
@@ -111,7 +111,7 @@ export default {
       searchText: '',
       topScrollBtnHide: true,
       isLoading: false,
-      collectTipShow: false,
+      collectTipHide: true,
       countyPharmacyNameData: {}
     }
   },
@@ -203,10 +203,10 @@ export default {
         setTimeout(() => {
           const collectPharmacy = []
           if (this.localStoragePharmacyData.length === 0) { //* 沒資料的話，後面的迴圈就不跑了
-            this.collectTipShow = true
+            this.collectTipHide = false
             return
           } else {
-            this.collectTipShow = false
+            this.collectTipHide = true
           }
           this.localStoragePharmacyData.forEach(pharmacyAddress => {
             this.rapidTestTempData.forEach(allPharmacyItem => {
