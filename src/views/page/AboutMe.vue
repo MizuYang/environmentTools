9f4458003886ed4f9cd986e26b244cb92faa78b6
mizu <psdk55052004@gmail.com>
2022-05-21 09:54:27 +0800

    新增 關於我頁面裡面的小彩蛋，展示天氣 icons 、紫外線危險級數的顏色列表，update_12 氣象、疫情查詢 2022/5/21

--- src/views/page/AboutMe.vue
+++ src/views/page/AboutMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about mt-3 mb-9 text-center">
+  <div class="about mt-3 mb-4 text-center">
     <div class="container w-low">
       <img src="https://github.com/MizuYang/environmentTools/blob/main/src/assets/image/cat.jpg?raw=true" class="mb-3" alt="貓咪害羞圖" height="180">
       <a href="https://equatorial-alloy-23b.notion.site/f6226e0c3e42458898d5cbdda240f6b6" target="_blank" class="btn btn-outline-primary d-block mb-3">觀看開發紀錄</a>
@@ -9,10 +9,34 @@
     <div class=" mb-2"><h1 class="text-style">你這樣偷看我，我會害羞啦</h1></div>
     <div class=" mb-2"><p class="text-style">此作品為練習使用<br>無任何商業用途</p></div>
     <div class=" mb-2"><a href="https://www.flaticon.com/" class="text-style" target="_blank">icons 來源</a></div>
-
-    <br>
   </div>
+
+  <h3 class="text-center">小彩蛋</h3>
+  <ul class="d-flex justify-content-center flex-wrap w-100 text-center px-2">
+    <li class="px-2" v-for="(weatherUrl, keys) in weatherIconsData" :key="keys">
+      <img :src="weatherUrl" :alt="`${keys}的 icon 圖`" height="35">
+      <p class="my-2">{{ keys }}</p>
+    </li>
+  </ul>
+  <ul class=" mb-10 text-center px-0">
+    <li class="mb-1" :class="`uv-lv${index + 1}`" v-for="(ulDangerLv, index) in ulDangerLvData" :key="`ulDangerLv${ulDangerLv}`">紫外線指數：{{ $uvDangerLv(ulDangerLv) }}</li>
+  </ul>
 </template>
+
+<script>
+import WeatherMixins from '@/components/weather/mixins/WeatherMixins.vue'
+export default {
+
+  data () {
+    return {
+      ulDangerLvData: [1, 3, 6, 8, 11]
+    }
+  },
+
+  mixins: [WeatherMixins]
+
+}
+</script>
 
 <style lang="scss" scoped>
 .text-style {
